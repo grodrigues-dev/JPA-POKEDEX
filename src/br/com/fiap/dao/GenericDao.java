cc2352d1e243ec58701adb1b312304d61a8ca8d8
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2020-04-08 15:28:02 -0300

    Criando genericDAO e exceções

---
+++ src/br/com/fiap/dao/GenericDao.java
@@ -0,0 +1,14 @@
+package br.com.fiap.dao;
+
+import br.com.fiap.exceptions.CommitException;
+import br.com.fiap.exceptions.ResourceNotFoundException;
+
+public interface GenericDao<T, K> {
+	void cadastrar(T entidade);
+	void atualizar(T entidade);
+	T pesquisar(K id);
+	
+	void deletar (K id) throws ResourceNotFoundException;
+	
+	void commit() throws CommitException;
+}
---
+++ src/br/com/fiap/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,30 @@
+package br.com.fiap.exceptions;
+
+public class ResourceNotFoundException extends Exception {
+
+	public ResourceNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResourceNotFoundException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResourceNotFoundException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResourceNotFoundException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResourceNotFoundException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
